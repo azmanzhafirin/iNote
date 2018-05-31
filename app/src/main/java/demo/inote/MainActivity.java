093110c099e04cdaa24f38e043ced781d2c6ddb3
dharma <dharmaseelan970@gmail.com>
2018-05-31 23:05:45 +0800

    new updates of inotes

--- app/src/main/java/demo/inote/MainActivity.java
+++ app/src/main/java/demo/inote/MainActivity.java
@@ -4,6 +4,7 @@ package demo.inote;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,20 +16,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //test
 
-        Button btn = (Button)findViewById(R.id.button_newNotes);
-        Button btn2 = (Button)findViewById(R.id.button_myNotes);
+//        Button btn = (Button)findViewById(R.id.button_newNotes);
+//        Button btn2 = (Button)findViewById(R.id.button_myNotes);
+//
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, NewNoteActivity.class));
+//            }
+//        });
+//
+//        btn2.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                startActivity(new Intent(MainActivity.this, MyNotes.class));
+//            }
+//        });
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        CardView cvNew=findViewById(R.id.create);
+            cvNew.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(),NewNoteActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+        CardView cvnotes=findViewById(R.id.mynotes);
+        cvnotes.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, NewNoteActivity.class));
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MyNotes.class);
+                startActivity(intent);
             }
         });
 
-        btn2.setOnClickListener(new View.OnClickListener(){
+        CardView dashboard=findViewById(R.id.dashboard);
+        dashboard.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                startActivity(new Intent(MainActivity.this, MyNotes.class));
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),NewNoteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        CardView calender=findViewById(R.id.calender);
+        calender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),NewNoteActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/demo/inote/MyNotes.java
+++ app/src/main/java/demo/inote/MyNotes.java
@@ -1,11 +1,24 @@
 package demo.inote;
 
+import android.content.DialogInterface;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -23,7 +36,7 @@ public class MyNotes extends AppCompatActivity {
     ListView listViewNotes;
     List<Notes> notesList;
     DatabaseReference databaseNotes;
-    TextView category, notes, rating;
+    TextView category;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +48,66 @@ public class MyNotes extends AppCompatActivity {
         listViewNotes = (ListView) findViewById(R.id.listViewNotes);
         category = (TextView) findViewById(R.id.textViewCategory);
         notesList = new ArrayList<>();
+
+
+        listViewNotes.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Notes notes = notesList.get(i);
+                showUpdateDialog(notes.getNoteId(), notes.getNote(), notes.getRating());
+                return false;
+            }
+        });
+
     }
+
+
+    private void showUpdateDialog(final String noteId, String noteName, String noteRating){
+
+        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = getLayoutInflater();
+
+        final View dialogView = inflater.inflate(R.layout.update_note, null);
+
+        dialogBuilder.setView(dialogView);
+
+        final EditText editTextCategory = (EditText) dialogView.findViewById(R.id.editTextCategory);
+        final Button buttonDelete = (Button) dialogView.findViewById(R.id.buttonDelete);
+        final Button buttonCancel = (Button) dialogView.findViewById(R.id.buttonCancel);
+
+        dialogBuilder.setTitle("Delete Confirmation");
+        dialogBuilder.setMessage("Are you sure you want to delete this note?");
+        dialogBuilder.setIcon(R.drawable.icon_delete2);
+        //dialogBuilder.setCancelable(true);
+
+        final AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteNote(noteId);
+
+            }
+        });
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+            }
+        });
+
+    }
+
+    private void deleteNote(String noteId) {
+        DatabaseReference drNote = FirebaseDatabase.getInstance().getReference("notes").child(noteId);
+        drNote.removeValue();
+        Toast.makeText(this, "Note is deleted", Toast.LENGTH_LONG).show();
+    }
+
 
     @Override
     protected void onStart() {
@@ -45,15 +117,13 @@ public class MyNotes extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 notesList.clear();
                 for(DataSnapshot noteSnapshot : dataSnapshot.getChildren()){
-                //    Map<String, String> map = (Map<String, String>) dataSnapshot.getValue();
-                  //  Log.d("test", "map" + map);
+                    //    Map<String, String> map = (Map<String, String>) dataSnapshot.getValue();
+                    //  Log.d("test", "map" + map);
 
-
-
-                 //  String category2 = map.get(map);
-                //   Log.d("LOOK HERE", "hola" +category2);
-                   //category.setText(category2);
-                   Notes notes = noteSnapshot.getValue(Notes.class);
+                    //  String category2 = map.get(map);
+                    //   Log.d("LOOK HERE", "hola" +category2);
+                    //category.setText(category2);
+                    Notes notes = noteSnapshot.getValue(Notes.class);
                     notesList.add(notes);
 
                 }
@@ -68,4 +138,6 @@ public class MyNotes extends AppCompatActivity {
             }
         });
     }
+
+
 }
